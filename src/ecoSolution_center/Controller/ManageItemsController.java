9b2584d2dcdbe61de49a2ec02461f37eea0178ab
kovac <kovacsbela958@gmail.com>
2020-06-08 11:06:42 +0200

    set the methods in the ManageItemsContrller class and the DBMethods

--- src/ecoSolution_center/Controller/ManageItemsController.java
+++ src/ecoSolution_center/Controller/ManageItemsController.java
@@ -1,8 +1,16 @@
 package ecoSolution_center.Controller;
 
+import ecoSolution_center.Data.DBMethods;
+import ecoSolution_center.Model.LaundryItem;
+import ecoSolution_center.Model.Status;
 import ecoSolution_center.View.UIControl.Controller;
 import ecoSolution_center.View.UIControl.windows;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +19,35 @@ public class ManageItemsController implements Initializable, windows {
 
     Controller myController;
 
+    private DBMethods dbMethods = new DBMethods();
+
+
+    public static ObservableList<LaundryItem> laundryItems = FXCollections.observableArrayList();
+    private String itemID = "";
+
+    private String description = dbMethods.getDescription();
+    private String itemStatus = dbMethods.getItemStatus();
+    private String clothingTypeName = dbMethods.getClothingTypeName();
+    private int laundryItemID = dbMethods.getLaundryItemID();
+
+
+    @FXML
+    private TextField itemID_textfield;
+    @FXML
+    private Label description_label,clothingType_label,itemID_label,itemStatus_label,orderStatus_label;
+    @FXML
+    private TableView<LaundryItem> order_tableview;
+    @FXML
+    private TableColumn<LaundryItem, String> description_column;
+    @FXML
+    private TableColumn<LaundryItem, String> itemStatus_column;
+    @FXML
+    private TableColumn<LaundryItem, String> clothingType_column;
+    @FXML
+    private TableColumn<LaundryItem, Integer> itemID_column;
+    @FXML
+    private ChoiceBox<String> itemStatus_choiceBox, orderStatus_choiceBox;
+
     @Override
     public void setScreenParent(Controller screenPage) {
         myController = screenPage;
@@ -18,6 +55,44 @@ public class ManageItemsController implements Initializable, windows {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //this fills the choice boxes
+        itemStatus_choiceBox.getItems().addAll(Status.dirtyInShop,Status.cleanInShop,Status.completed);
+        itemStatus_choiceBox.setValue(Status.dirtyInShop);
+        orderStatus_choiceBox.getItems().addAll(Status.dirtyInShop,Status.cleanInShop,Status.completed);
+        orderStatus_choiceBox.setValue(Status.dirtyInShop);
+
+        //these are the columns in the table view
+        description_column.setCellValueFactory(new PropertyValueFactory<LaundryItem, String>("description"));
+        itemStatus_column.setCellValueFactory(new PropertyValueFactory<LaundryItem, String>("itemStatus"));
+        clothingType_column.setCellValueFactory(new PropertyValueFactory<LaundryItem, String>("clothingTypeName"));
+        itemID_column.setCellValueFactory(new PropertyValueFactory<LaundryItem, Integer>("laundryItemID"));
+
+
+    }
+
+
+    public void go() {
+        order_tableview.getItems().clear();
+        itemID = itemID_textfield.getText();
+        dbMethods.selectLaundryItems(Integer.parseInt(itemID));
+        order_tableview.setItems(laundryItems);
+        dbMethods.selectLaundryItemDetails(Integer.parseInt(itemID));
+        dbMethods.selectOrderStatus(Integer.parseInt(itemID));
+        description_label.setText("Description: " + dbMethods.getDescription());
+        itemID_label.setText("Item ID: " + itemID_textfield.getText());
+        clothingType_label.setText("Clothing type: " + dbMethods.getClothingTypeName());
+        itemStatus_label.setText(dbMethods.getItemStatus());
+        orderStatus_label.setText(dbMethods.getOrderStatus());
+
+
+
+
+    }
+
+    public void setStatuses(){
+        itemID = itemID_textfield.getText();
+        dbMethods.updateOrderStatus(Integer.parseInt(itemID), orderStatus_choiceBox.getValue());
+        dbMethods.updateItemStatus(Integer.parseInt(itemID), itemStatus_choiceBox.getValue());
 
     }
 }
--- src/ecoSolution_center/Data/DBMethods.java
+++ src/ecoSolution_center/Data/DBMethods.java
@@ -1,6 +1,138 @@
 package ecoSolution_center.Data;
 
+import ecoSolution_center.Controller.ManageItemsController;
+import ecoSolution_center.Model.LaundryItem;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DBMethods {
+    ////////////////////////////////////////fields////////////////////////////////////
+    private String name;
+    private String phoneNo;
+    private String description = "";
+    private String itemStatus = "";
+    private String clothingTypeName = "";
+    private String orderStatus = "";
+    private int laundryItemID = 0;
+
+
+    //////////////////////////////Methods////////////////////////////////////////
+
+
+    public void updateOrderStatus(int laundryItemID, String orderStatus){
+        try {
+            PreparedStatement query = DBConnection.getConnect().prepareStatement("update tblOrder \n" +
+                    "set fldOrderStatus = ? \n" +
+                    "where fldOrderID = (select fldOrderID from tblLaundryItem where fldLaundryItemID = ?)");
+            query.setString(1, orderStatus);
+            query.setInt(2, laundryItemID);
+            query.executeUpdate();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void updateItemStatus(int laundryItemID, String itemStatus){
+        try {
+            PreparedStatement query = DBConnection.getConnect().prepareStatement("update tblLaundryItem\n" +
+                    "set fldItemStatus = ? \n" +
+                    "where fldLaundryItemID = ? ");
+            query.setString(1, itemStatus);
+            query.setInt(2, laundryItemID);
+            query.executeUpdate();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void selectOrderStatus(int laundryItemID){
+        try {
+            PreparedStatement query = DBConnection.getConnect().prepareStatement("SELECT fldOrderStatus \n" +
+                    "FROM tblOrder \n" +
+                    "WHERE fldOrderID IN (SELECT fldOrderID FROM tblLaundryItem WHERE fldLaundryItemID = ?) ");
+            query.setInt(1, laundryItemID);
+            ResultSet resultSet = query.executeQuery();
+            if (resultSet.next()) {
+                orderStatus = resultSet.getString(1);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void selectLaundryItemDetails( int laundryItemID){
+        try {
+            PreparedStatement query = DBConnection.getConnect().prepareStatement("select * from tblLaundryItem where fldLaundryItemID = ? ");
+            query.setInt(1, laundryItemID);
+            ResultSet resultSet = query.executeQuery();
+            if (resultSet.next()) {
+                description = resultSet.getString(2);
+                itemStatus = resultSet.getString(5);
+                clothingTypeName = resultSet.getString(4);
+                laundryItemID = resultSet.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void selectLaundryItems(int itemID){
+        try {
+            PreparedStatement query = DBConnection.getConnect().prepareStatement("SELECT * \n" +
+                    "  FROM tblLaundryItem \n" +
+                    " WHERE fldOrderID IN (SELECT fldOrderID\n" +
+                    "                FROM tblLaundryItem\n" +
+                    "               WHERE fldLaundryItemID = ?) ");
+            query.setInt(1, itemID);
+            ResultSet resultSet = query.executeQuery();
+            while(resultSet.next()){
+                System.out.println(resultSet.getString(2)+resultSet.getInt(1)+
+                        resultSet.getString(5)+resultSet.getString(4));
+                ManageItemsController.laundryItems.add(new LaundryItem(resultSet.getString(2),resultSet.getInt(1),
+                        resultSet.getString(5),resultSet.getString(4)));
+                //System.out.println(CheckOrderController.laundryItems.get() + "  list");
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+//////////////////////////////////////getters and setters/////////////////////////
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getItemStatus() {
+        return itemStatus;
+    }
+
+    public String getClothingTypeName() {
+        return clothingTypeName;
+    }
+
+    public String getOrderStatus() {return orderStatus;}
+
+    public int getLaundryItemID() {
+        return laundryItemID;
+    }
 
 
 }
